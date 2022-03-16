5ed380033abed4fe413e47d37349e8a4a99d8eee
Manny <vmlujanjr@outlook.com>
2022-03-16 12:15:25 -0500

    checking for decprecated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new box!";
     tasksToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+
+console.log(createTaskHandler());
 
